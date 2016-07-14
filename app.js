a7f1303276cf75c798d47faf8f1703c1e0cdd816
Paul Sweeney Jr <paul@sweeneyapps.com>
2016-07-13 21:10:36 -0700

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-function newTab() {
-    console.log("link clicked");
-    chrome.tabs.create({ url: "http://deafinitelymag.com" }, function (t) { console.log(t.id); });
-
-}
-
-document.querySelector("#clickable").onclick = newTab;
